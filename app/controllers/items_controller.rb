d2d6799a765810e182bf9aa2ff8a690687b1b594
Petru Madar <petrumadar@gmail.com>
2020-04-06 01:24:09 +0300

    Add Item#show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,21 +1,21 @@
 class ItemsController < ApplicationController
-    def new
-        @item = Item.new
-    end
+  def new
+    @item = Item.new
+  end
 
-    def create
-        @item = Item.new(item_params)
-        @item.save
-        redirect_to @item
-        #render plain: params[:item].inspect
-    end
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to @item
+    #render plain: params[:item].inspect
+  end
 
-    def show
+  def show
+    @item = Item.find(params[:id])
+  end
 
-    end
-
-    private
-    def item_params
-        params.require(:item).permit(:title,:owned_since,:info,:broken,:location)
-    end
+  private
+  def item_params
+    params.require(:item).permit(:title, :owned_since, :info, :broken, :location)
+  end
 end
