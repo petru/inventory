16a5808b3926fbd48df09f12f6208a69e61c2c19
Petru Madar <petrumadar@gmail.com>
2020-04-06 04:27:41 +0300

    Refine date_select

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,10 +27,17 @@ class ItemsController < ApplicationController
   def update
     @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to @item
+      redirect_to items_path
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    redirect_to items_path
   end
   
   private
