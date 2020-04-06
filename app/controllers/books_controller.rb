a96ec8a75d8a02356930c84423ebd5e78a2bd377
Petru Madar <petrumadar@gmail.com>
2020-04-07 01:14:46 +0300

    Add Book#destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,6 +33,13 @@ class BooksController < ApplicationController
     end
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path
+  end
+
   private
   def book_params
     params.require(:book).permit(:title, :author, :year, :genre, :owned_since, :format, :read, :notes)
