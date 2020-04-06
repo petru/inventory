d15e139ac82ed0d45c728885e930d7b107d8595c
Petru Madar <petrumadar@gmail.com>
2020-04-07 01:23:11 +0300

    Add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true, length: { minimum: 5 }
+  validates :year, presence: true, length: { is: 4 }, numericality: { only_integer: true }
 end
